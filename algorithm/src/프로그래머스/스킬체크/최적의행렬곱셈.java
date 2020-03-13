0def934c7ece850786d9cb945628e518064a4b80
beomhyun <beomhyunkim@gmail.com>
2020-03-13 17:52:17 +0900

    dp..?

---
+++ algorithm/src/프로그래머스/스킬체크/최적의행렬곱셈.java
@@ -0,0 +1,11 @@
+package 프로그래머스.스킬체크;
+
+public class 최적의행렬곱셈 {
+	public int solution(int[][] matrix_sizes) {
+		int length = matrix_sizes.length;
+		int[][] m = new int[length][length];
+		
+		int answer = 0;
+		return answer;
+	}
+}
