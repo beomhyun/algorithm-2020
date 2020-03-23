fdcc6085b1402d67b68d83ee9e304d5df3e9bf39
beomhyun <beomhyunkim@gmail.com>
2020-03-23 16:07:18 +0900

    트리공부해야해

---
+++ algorithm/src/트리/전위순회이진트리.java
@@ -0,0 +1,21 @@
+package 트리;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 전위순회이진트리 {
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(3);
+		list.add(4);
+		list.add(2);
+		
+		solution(list);
+	}
+	public static boolean solution(List<Integer> list) {
+		//루트 -> 왼쪽(루트보다작음) -> 오른쪽(루트보다 큼)
+		
+		return false;
+	}
+}
