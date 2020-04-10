8979b1cbdb03be2c54cef950b23357095acd9c85
beomhyun <beomhyunkim@gmail.com>
2020-04-10 16:49:42 +0900

    호텔방배정 인터넷 참고

--- algorithm/src/카카오2019겨울인턴/호텔방배정.java
+++ algorithm/src/카카오2019겨울인턴/호텔방배정.java
@@ -1,32 +1,46 @@
 package 카카오2019겨울인턴;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class 호텔방배정 {
-	public long[] solution(long k, long[] room_number) {
+	public static long[] solution(long k, long[] room_number) {
 		long[] answer = new long[room_number.length];
-		Set<Long> set = new HashSet();
-		for (int i = 0; i < answer.length; i++) {
-			answer[i] = findRoom(set, room_number[i], k);
+		Map<Long, Long> map = new HashMap<>();
+		for (int i = 0; i < room_number.length; i++) {
+			long tmp = room_number[i];
+			List<Long> list = new ArrayList<Long>();
+
+			if (!map.containsKey(tmp)) {
+				map.put(tmp, tmp + 1);
+				answer[i] = tmp;
+			} else {
+				long parent = map.get(tmp);
+				list.add(parent);
+				while (true) {
+					if (!map.containsKey(parent)) {
+						map.put(parent, parent + 1);
+						answer[i] = parent;
+						break;
+					} else {
+						list.add(parent);
+						parent = map.get(parent);
+					}
+				}
+				for (long l : list) {
+					map.put(l, parent + 1);
+				}
+			}
+
 		}
 		return answer;
 	}
 
-	public static long findRoom(Set<Long> set, long wanted, long k) {
-		if (!set.contains(wanted)) {
-			set.add(wanted);
-			return wanted;
-		}
-		long index = wanted;
-		while (index <= k) {
-			if (!set.contains(index)) {
-				set.add(index);
-				return index;
-			} else {
-				index++;
-			}
-		}
-		return 0;
+	public static void main(String[] args) {
+		long k = 10;
+		long[] room_number = { 1, 3, 4, 1, 3, 1 };
+		System.out.println(solution(k, room_number));
 	}
 }
